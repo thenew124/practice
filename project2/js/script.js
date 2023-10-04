69242b43941a560c07039e6b570a751c7aa96546
thenew124 <142332158+thenew124@users.noreply.github.com>
2023-10-04 10:44:09 -0700

    Add files via upload

---
+++ project2/js/script.js
@@ -0,0 +1,57 @@
+console.log("From index html");
+let fname= "Adebayo".toUpperCase();
+let lname= "Talogbon";
+console.log(fname," ",lname);
+fname= "ope";
+lname="Buju";
+console.log(fname, " ", lname)
+
+// number
+let three = 3;
+let four = 4;
+let seven = three + four + 10;
+console.log (seven);
+
+//Booolean
+let t = true;
+let f = false;
+console.log(t, f)
+
+
+//object
+let person = {};
+person.fname = "Adeola";
+person.lname = "Kajola";
+person.age = 5;
+person.dob = "11-19-1997"
+
+
+let person1 = {};
+person1.fname = "Peter";
+person1.lname = "Adejoke";
+person1.age = 9;
+person1.dob = "12-05-1992"
+console.log(person1.fname)
+
+// array
+let numbers = [1,2,3,4,5,6]
+            // 0,1,2,3,4,5
+
+numbers.push(person1)
+let personInArray = numbers[6]
+
+
+console.log(personInArray)
+
+// numbers.pop()
+console.log(numbers)
+/*
+1.assignment= learn the methods associated with strings.
+2.methods associated with numbers.
+3.methods associated with objects.
+4.methods associated with array.
+
+
+
+*/
+
